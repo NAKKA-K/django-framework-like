36b7331b5ecc6b8f3040e0b67f6ab2ed7f351f15
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-02-11 16:50:09 +0000

    Add WSGIHandler

---
+++ django-framework-like/core/wsgi.py
@@ -0,0 +1,17 @@
+class WSGIHandler:
+    """WSGI application class"""
+
+    request_class = WSGIRequest
+
+    def __call__(self, environ, start_response):
+        """WSGI interface"""
+        request = self.request_class(environ)
+        response = self.get_response(request)
+        status = '{} {}'.format(response.status_code, response.reason_phrase)
+        start_response(status, response.headers)
+        return response
+
+    def get_response(self, request):
+        """call view, and return HttpResponse object"""
+        from core.response import HttpResponse
+        return HttpResponse('Hello World')
